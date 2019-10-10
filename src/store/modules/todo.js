91e9ba1409aaac05cc5e31846b8d5264d37f7e73
Ssnnaaiill <newdk3025@gmail.com>
2019-10-10 20:13:03 +0900

    add actions for features in todo component

--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -1,12 +1,12 @@
 import { createAction } from 'redux-actions';
-
+import { Map, List } from 'immutable';
 
 /**
  * Actions for todo list component
  * - CHANGE_INPUT: change input value of todo item
- * - INSERT: add new todo item
- * - TOGGLE: add or remove strikethrough of todo item
- * - REMOVE: remove todo item
+ * - INSERT      : add new todo item
+ * - TOGGLE      : add or remove strikethrough of todo item
+ * - REMOVE      : remove todo item
  */
 
 const CHANGE_INPUT = 'todo/CHANGE_INPUT';
@@ -17,12 +17,37 @@ const REMOVE = 'todo/REMOVE';
 
 /**
  * createAction
- * @param
- * @param
- * @param
+ * @param actionName    : set action name
+ * @param payloadCreator: set payload (default: createAction parameter)
+ * @param metaCreator   : set meta
  */
 
-export const changeInput = createAction(CHANGE_INPUT);
-export const insert = createAction(INSERT);
-export const toggle = createAction(TOGGLE);
-export const remove = createAction(REMOVE);
+export const changeInput = createAction(CHANGE_INPUT, value => value);
+export const insert = createAction(INSERT, text => text);
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
+
+let id = 0;
+
+const initialState = Map({
+  input: '',
+  todos: List()
+});
+
+export default handleActions({
+  [CHANGE_INPUT]: (state, action) => {
+    state.set('input', action.payload)
+  },
+  [INSERT]: (state, { payload: text }) => {
+    const item = Map({ id: id++, checked: false, text });
+    return state.update('todos', todos => todos.push(item));
+  },
+  [TOGGLE]: (state, { payload: id }) => {
+    const index = state.get('todos').findIndex(item => item.get('id') === id);
+    return state.updateIn(['todos', index, 'checked'], checked => !checked);
+  },
+  [REMOVE]: (state, { payload: id }) => {
+    const index = state.get('todos').findIndex(item => item.get('id') === id);
+    return state.deleteIn(['todos', index]);
+  }
+}, initialState);
